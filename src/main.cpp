4b611e34af2f4e3e8fcb6c984586e3e6e76a3ce5
Pedro Moreira <pcbmoreira@id.uff.br>
2024-06-06 02:07:22 -0300

    Adapted frequency counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,9 @@
 #include <Arduino.h>
 
-#define CODE 1
-
-#if CODE == 0
-
 constexpr int frequencyPin = GPIO_NUM_35;
 
 volatile unsigned long lastMeasure = 0;
-volatile unsigned long period = 0;
-
-void Frequency_ISR()
-{
-	unsigned long current = micros();
-	period = current - lastMeasure;
-	lastMeasure = current;
-}
-
-void setup()
-{
-	Serial.begin(9600);
-	pinMode(frequencyPin, INPUT);
-
-	attachInterrupt(digitalPinToInterrupt(frequencyPin), Frequency_ISR, RISING);
-
-	delay(1000);
-}
-
-void loop()
-{
-	float frequency;
-	if (!period)
-		frequency = 0;
-	else
-		frequency = 1000000.0 / period;
-
-	Serial.printf("Frequency: %.2f\t\r", frequency);
-}
-
-#endif
-
-#if CODE == 1
-
-constexpr int frequencyPin = GPIO_NUM_35;
-
-volatile unsigned long lastMeasure = 0;
-volatile unsigned long period = 0;
+volatile unsigned long periodSum = 0;
 
 volatile unsigned int counter = 0;
 
@@ -54,15 +13,14 @@ unsigned long lastTime = 0;
 void Frequency_ISR()
 {
 	unsigned long current = micros();
-	period = current - lastMeasure;
+	periodSum += current - lastMeasure;
 	lastMeasure = current;
-
 	counter++;
 }
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(115200);
 	pinMode(frequencyPin, INPUT);
 
 	attachInterrupt(digitalPinToInterrupt(frequencyPin), Frequency_ISR, RISING);
@@ -75,9 +33,11 @@ void loop()
 	if (millis() - lastTime >= timer)
 	{
 		lastTime = millis();
-		Serial.printf("Frequency: %d\n", counter);
+
+		float period = (float)periodSum / counter;
+		float frequency = 1000000.0/period;
+		Serial.printf("Frequency: %f    Measures: %d\n", frequency, counter);
 		counter = 0;
+		periodSum = 0;
 	}
 }
-
-#endif
